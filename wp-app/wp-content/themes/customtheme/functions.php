aaf3d6efc9fbc7f0d7f0512b934ae7c578be8a1b
truonghoangngoclam <truong.lam@marketenterprise.vn>
2023-01-15 10:39:54 +0700

    add some plugin for debug

--- wp-app/wp-content/themes/customtheme/functions.php
+++ wp-app/wp-content/themes/customtheme/functions.php
@@ -29,8 +29,8 @@ function ngoclam_register_styles() {
 
 function ngoclam_register_scripts() {
 	$version = wp_get_theme()->get("version");
-	wp_enqueue_script("jquery", "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), "3.4.1", true);
-	wp_enqueue_script("popper", "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), "1.16.0", true);
+	wp_enqueue_script("jquery", "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), "3.4.1", false);
+	wp_enqueue_script("popper", "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), "1.16.0", false);
 	wp_enqueue_script("bootstrap", "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), "4.4.1", true);
 	wp_enqueue_script("custom-js", get_template_directory_uri() . '/assets/js/main.js', array(), $version, true);
 }
--- wp-app/wp-content/themes/customtheme/template-parts/content-archive.php
+++ wp-app/wp-content/themes/customtheme/template-parts/content-archive.php
@@ -1,13 +1,13 @@
 <div class="container">
     <div class="post mb-5">
         <div class="media">
-            <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url("thumbnail")?>" alt="image">
+            <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url("thumbnail") ?>" alt="image">
             <div class="media-body">
-                <h3 class="title mb-1"><a href="post.html"><?php the_title() ?></a></h3>
+                <h3 class="title mb-1"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
                 <div class="meta mb-1">
                     <span class="date"><?php the_date() ?></span>
                     <span class="comment">
-                        <a href="#"><?php comments_number()?> Bình luận</a>
+                        <a href="#"><?php comments_number() ?> Bình luận</a>
                     </span>
                 </div>
                 <div class="intro">
@@ -15,7 +15,7 @@
                     the_excerpt();
                     ?>
                 </div>
-                <a class="more-link" href="<?php the_permalink()?>">Read more &rarr;</a>
+                <a class="more-link" href="<?php the_permalink() ?>">Read more &rarr;</a>
             </div><!--//media-body-->
         </div><!--//media-->
     </div>
