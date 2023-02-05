8aa7e355f00399f55c5306a09af792942e1588e6
truonghoangngoclam <truong.lam@marketenterprise.vn>
2023-02-05 16:50:51 +0700

    feat: add bootstrap and custom header for theme jay-basic-shop

--- wp-app/wp-content/themes/jay-basic-shop/functions.php
+++ wp-app/wp-content/themes/jay-basic-shop/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * jay-basic-shop functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package jay-basic-shop
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function jay_basic_shop_setup() {
+function jay_basic_shop_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on jay-basic-shop, use a find and replace
 		* to change 'jay-basic-shop' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'jay-basic-shop', get_template_directory() . '/languages' );
+	load_theme_textdomain('jay-basic-shop', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +39,19 @@ function jay_basic_shop_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'jay-basic-shop' ),
+			'menu-1' => esc_html__('Primary', 'jay-basic-shop'),
 		)
 	);
 
@@ -83,7 +85,7 @@ function jay_basic_shop_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +102,7 @@ function jay_basic_shop_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'jay_basic_shop_setup' );
+add_action('after_setup_theme', 'jay_basic_shop_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +111,24 @@ add_action( 'after_setup_theme', 'jay_basic_shop_setup' );
  *
  * @global int $content_width
  */
-function jay_basic_shop_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'jay_basic_shop_content_width', 640 );
+function jay_basic_shop_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('jay_basic_shop_content_width', 640);
 }
-add_action( 'after_setup_theme', 'jay_basic_shop_content_width', 0 );
+add_action('after_setup_theme', 'jay_basic_shop_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function jay_basic_shop_widgets_init() {
+function jay_basic_shop_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'jay-basic-shop' ),
+			'name'          => esc_html__('Sidebar', 'jay-basic-shop'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'jay-basic-shop' ),
+			'description'   => esc_html__('Add widgets here.', 'jay-basic-shop'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +136,41 @@ function jay_basic_shop_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'jay_basic_shop_widgets_init' );
+add_action('widgets_init', 'jay_basic_shop_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function jay_basic_shop_scripts() {
-	wp_enqueue_style( 'jay-basic-shop-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'jay-basic-shop-style', 'rtl', 'replace' );
+function jay_basic_shop_scripts()
+{
+	wp_enqueue_style('jay-basic-shop-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_enqueue_style('jay-basic-shop-main-style', get_template_directory_uri() . "/css/main.css", array(), _S_VERSION);
+	wp_enqueue_style('bootstrap_styles', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css", array(), "5.3.0");
+	wp_enqueue_style('bootstrap_icons_styles', "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css", array(), "1.10.3");
+	wp_style_add_data('jay-basic-shop-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'jay-basic-shop-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('jay-basic-shop-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+	wp_enqueue_script('popper_for_bootstrap', "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js", array(), "2.11.6", true);
+	wp_enqueue_script('bootstrap_scripts', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js", array(), "5.3.0", true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'jay_basic_shop_scripts' );
+add_action('wp_enqueue_scripts', 'jay_basic_shop_scripts');
+
+/**
+ * Custom fonts
+ * font-family: 'Nunito', sans-serif;
+ */
+function enqueue_custom_fonts()
+{
+	if (!is_admin()) {
+		wp_register_style("nunito", "https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Rubik:wght@400;500;600;700&display=swap");
+		wp_enqueue_style("nunito");
+	}
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_fonts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +195,24 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Show cart contents / total Ajax
+ */
+add_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
+
+function woocommerce_header_add_to_cart_fragment($fragments)
+{
+	global $woocommerce;
+
+	ob_start();
+
+?>
+	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count); ?> – <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+<?php
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
