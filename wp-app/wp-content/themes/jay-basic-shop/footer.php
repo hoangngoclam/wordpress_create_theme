0339b2fbe822dd4adcb01f0fb5641725732a546a
truonghoangngoclam <truong.lam@marketenterprise.vn>
2023-02-05 18:01:44 +0700

    feat: update layout toppage + add images

--- wp-app/wp-content/themes/jay-basic-shop/footer.php
+++ wp-app/wp-content/themes/jay-basic-shop/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -6,29 +7,51 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package jay-basic-shop
+ * @package pawsgang
  */
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jay-basic-shop' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'jay-basic-shop' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jay-basic-shop' ), 'jay-basic-shop', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+
+	<div class="bg-primary text-white pt-5 pb-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-md-2">
+					<?php dynamic_sidebar('footer-widget-col-one'); ?>
+				</div>
+
+				<div class="col-sm-6 col-md-2">
+					<?php dynamic_sidebar('footer-widget-col-two'); ?>
+				</div>
+
+				<div class="col-md-4 col-sm-12 ms-auto">
+					<?php dynamic_sidebar('footer-widget-col-three'); ?>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+
+	<div class="container pt-2 pb-2">
+		<div class="row d-flex align-items-center">
+			<div class="col">
+				<p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?> / Created by <a href="https://raddy.co.uk" target="_blank">Raddy</a>
+			</div>
+			<div class="col h-25 d-inline-block text-end">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/payment-methods.png" class="img-fluid" loading="lazy" alt="...">
+			</div>
+		</div>
+	</div>
+
+
+
+</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
