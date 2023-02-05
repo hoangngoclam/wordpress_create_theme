3524e1eb8c0ac7d7ff1b00081afe645fa77f0a05
truonghoangngoclam <truong.lam@marketenterprise.vn>
2023-02-05 15:23:37 +0700

    feat: add woocomerce for wp + create new theme

---
+++ wp-app/wp-content/themes/jay-basic-shop/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * jay-basic-shop Theme Customizer
+ *
+ * @package jay-basic-shop
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function jay_basic_shop_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'jay_basic_shop_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'jay_basic_shop_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'jay_basic_shop_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function jay_basic_shop_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function jay_basic_shop_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function jay_basic_shop_customize_preview_js() {
+	wp_enqueue_script( 'jay-basic-shop-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'jay_basic_shop_customize_preview_js' );
